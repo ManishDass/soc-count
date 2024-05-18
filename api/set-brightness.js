d739e37a3fae93f7b4030fe1f162be703ff81517
Manish <manishdas11811480@gmail.com>
2024-05-18 20:37:29 +0530

    changed IP

--- api/set-brightness.js
+++ api/set-brightness.js
@@ -3,7 +3,7 @@
 export default async function handler(req, res) {
     const { brightness } = req.query;
     const apiKey = 'ae55df63-7f1f-4c41-be1e-c1bfcb1a4a7e'; // Replace with your API key
-    const esp8266IpAddress = 'http://117.209.99.150:80'; // Replace with your ESP8266 IP address
+    const esp8266IpAddress = 'http://117.209.106.78:80'; // Replace with your ESP8266 IP address
   
     try {
       const response = await fetch(`${esp8266IpAddress}/set-brightness?brightness=${brightness}`, {
