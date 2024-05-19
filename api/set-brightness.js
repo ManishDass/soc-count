c6e1fe64f089fda1d96015159342dbbedb8ea22d
Manish <manishdas11811480@gmail.com>
2024-05-19 23:31:37 +0530

    Fix Local API Issues

--- api/set-brightness.js
+++ api/set-brightness.js
@@ -6,10 +6,10 @@ export default async function handler(req, res) {
    const esp8266IpAddress = process.env.REACT_APP_DDNS_IP; // DDNS cz my ISP Dont provide me static IP
   
     try {
-      const response = await fetch(`${esp8266IpAddress}/set-brightness?brightness=${brightness}`, {
+      const response = await fetch(`http://manishdas.ddns.net/set-brightness?brightness=${brightness}`, {
         method: 'GET',
         headers: {
-          'Authorization': `Bearer ${apiKey}`
+          'Authorization': `Bearer ae55df63-7f1f-4c41-be1e-c1bfcb1a4a7e`
         }
       });
       const data = await response.json();
