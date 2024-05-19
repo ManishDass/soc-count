eeca59347964c1e290fb84f360b864c827fda17c
Manish <manishdas11811480@gmail.com>
2024-05-19 22:05:10 +0530

    Fix Local API Issues

--- src/service/ApiESP.js
+++ src/service/ApiESP.js
@@ -7,10 +7,10 @@ const apiKey = process.env.REACT_APP_API_KEY; // API key
 // Function to set display type
 const setDisplayType = async (type) => {
   try {
-    const response = await fetch(`${apiUrl}/set-display-type?type=${type}`, {
+    const response = await fetch(`http://manishdas.ddns.net/api/set-display-type?type=${type}`, {
       method: 'GET',
       headers: {
-        'Authorization': `Bearer ${apiKey}`
+        'Authorization': `Bearer ae55df63-7f1f-4c41-be1e-c1bfcb1a4a7e`
       }
     });
     const data = await response.json();
